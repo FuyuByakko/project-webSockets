ae807a2a752cd621b5f6365d4270774538d9f994
Igor Michailov <igor.m.byak@gmail.com>
2019-11-24 19:35:43 +0900

    connection message

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http')
 const WebSocketServer = require("websocket").server;
-let connections = [];
 
 let httpServer = http.createServer((req, res) => {
   res.end("Request Received");
@@ -9,17 +8,7 @@ let httpServer = http.createServer((req, res) => {
 const wsServer = new WebSocketServer({"httpServer": httpServer});
 wsServer.on("request", request => {
   connection = request.accept(null, request.origin);
-  connections.push(connection);
-  // connection.on("open", () => {console.log("OPEN!")})
-  // connection.on("closed", () => {console.log("CLOSED!")})
-  connection.on("message", (message) => {
-    console.log(`received: ${message.utf8Data}`);
-    connections.forEach(connection => {
-      connection.send(message.utf8Data);
-    })
-  })
-  setInterval(() => {connection.send(new Date())}, 5000); 
-
+  console.log("New Connection Created!");
 })
 
 httpServer.listen(8080);
