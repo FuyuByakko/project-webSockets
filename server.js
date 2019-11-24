832a4f052f11d339b2f9086316841d6eaa67d70d
Igor Michailov <igor.m.byak@gmail.com>
2019-11-24 18:33:45 +0900

    added ws-server

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
 const http = require('http')
 const WebSocketServer = require("websocket").server;
-let connections = [];
 
 let httpServer = http.createServer((req, res) => {
   console.log('request received');
 })
 
 const wsServer = new WebSocketServer({"httpServer": httpServer});
-wsServer.on("request", request => {
-  connection = request.accept(null, request.origin);
-  connections.push(connection);
-  // connection.on("open", () => {console.log("OPEN!")})
-  // connection.on("closed", () => {console.log("CLOSED!")})
-  connection.on("message", (message) => {
-    console.log(`received: ${message.utf8Data}`);
-    connections.forEach(connection => {
-      connection.send(message.utf8Data);
-    })
-  })
-  setInterval(() => {connection.send(new Date())}, 5000); 
-
-})
 
 httpServer.listen(8080);
 console.log("OPENED on port 8080");
