c3146ee9effe1c46764bf679e52f79f1f8b937be
Igor Michailov <igor.m.byak@gmail.com>
2019-11-24 19:39:08 +0900

    interval

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const WebSocketServer = require("websocket").server;
+let connection;
 
 let httpServer = http.createServer((req, res) => {
   res.end("Request Received");
@@ -10,6 +11,8 @@ wsServer.on("request", request => {
   connection = request.accept(null, request.origin);
   console.log("New Connection Created!");
 })
+
+setInterval(() => {connection.send(new Date())})
 
 httpServer.listen(8080);
 console.log("OPENED on port 8080");
